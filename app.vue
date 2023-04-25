809c4349bc87038f6c89cf0e06c3f8c0a884735f
Beautus <48126337+sduduzog@users.noreply.github.com>
2023-04-25 10:47:55 +0000

    initial test

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    {{ something }}
+  </div>
+  <div v-html="rendered.html"></div>
+</template>
+
+<script setup>
+import mjml2html from 'mjml';
+import something from '@/assets/mjml/test.mjml?raw'
+
+const rendered = mjml2html(something)
+</script>
